e7131d8a16984038da113a5a5c1cf16c6924a6cc
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-01-11 12:37:50 -0700

    commit tree e2db0ab7c7de41b4d838ff9688ebe602fed73fff

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,48 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        Movie.create(title: title)
+        # movie = Movie.new(title: title)
+        # movie.save **to add to db
+        # movie **to return movie
+    end
 
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(attributes)
+        self.find_by(attributes)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where("release_date > 2002")
+    end
+
+    def update_with_attributes(attr)
+        self.update(attr)
+    end
+
+    def self.update_all_titles(title)
+        Movie.update(title: title)
+    end
+
+    def self.delete_by_id(id)
+        self.destroy(id)
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 end
